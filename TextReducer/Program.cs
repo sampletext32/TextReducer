3c350340c5e75f4463d5069e24aec2b4ce36af6c
BirdEgop <sampletext32@bk.ru>
2022-02-10 20:35:57 +0300

    Implement

---
+++ TextReducer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using TextReducer;
+
+string[] keywords =
+{
+    "должны", "должен", "должно", "необходимо", "необходимы", "обязательно", "обязательны", "обязательный",
+    "обязательные", "использовать", "надлежит"
+};
+
+Console.Write("Enter input file path: ");
+var inputPath = Console.ReadLine().Replace("\\", "/");
+if (!File.Exists(inputPath))
+{
+    Console.WriteLine("File not found");
+    Console.WriteLine("press any key to exit");
+    Console.ReadKey();
+    return;
+}
+
+var inputText = File.ReadAllText(inputPath);
+
+var outputBuilder = new StringBuilder();
+
+var paragraphs = inputText.Split("\n");
+
+foreach (var paragraph in paragraphs)
+{
+    if (paragraph.StartsWith("\t"))
+    {
+        var hasKeyword = paragraph
+            .ReplaceWithPredicate(' ', c => !char.IsLetterOrDigit(c))
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(w => w.ToLower()).Any(w => keywords.Contains(w));
+
+        if (hasKeyword)
+        {
+            outputBuilder.AppendLine(paragraph);
+        }
+    }
+}
+
+Console.WriteLine(outputBuilder.ToString());
---
+++ TextReducer/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace TextReducer;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Replaces chars in string input if they match the predicate
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="replacement"></param>
+    /// <param name="predicate"></param>
+    /// <returns></returns>
+    public static string ReplaceWithPredicate(this string input, char replacement, Func<char, bool> predicate)
+    {
+        return string.Create(input.Length, input, (span, s) =>
+        {
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (predicate(s[i]))
+                {
+                    span[i] = replacement;
+                }
+                else
+                {
+                    span[i] = s[i];
+                }
+            }
+        });
+    }
+}
